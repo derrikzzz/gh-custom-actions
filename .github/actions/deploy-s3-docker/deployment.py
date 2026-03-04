30039009f3e89ef51d7d84fcccd8cda6e32de5b0
Derrick Low <lowderrickym@gmail.com>
2026-03-04 18:27:03 +0800

    update casing of inputs

--- .github/actions/deploy-s3-docker/deployment.py
+++ .github/actions/deploy-s3-docker/deployment.py
@@ -4,8 +4,8 @@ from botocore.config import Config
 
 def deploy_s3_docker():
     bucket = os.environ.get('INPUT_BUCKET') #these are automatically generated variables
-    bucket_region = os.environ['INPUT_BUCKET_REGION']
-    dist_folder = os.environ['INPUT_DIST_FOLDER']
+    bucket_region = os.environ['INPUT_BUCKET-REGION']
+    dist_folder = os.environ['INPUT_DIST-FOLDER']
     
     configuration = Config(region_name=bucket_region)
     s3_client = boto3.client('s3', config=configuration) #automatically looks for environment variables with AWS credentials to authenticate outgoing AWS requests
