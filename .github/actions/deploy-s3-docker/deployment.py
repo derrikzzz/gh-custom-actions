130a1b16486c7420616b0c8e61dbb7d84c7382a5
Derrick Low <lowderrickym@gmail.com>
2026-03-04 13:59:37 +0800

    fix bugs and remove deprecated commands

--- .github/actions/deploy-s3-docker/deployment.py
+++ .github/actions/deploy-s3-docker/deployment.py
@@ -3,7 +3,7 @@ import boto3
 from botocore.config import Config
 
 def deploy_s3_docker():
-    bucket = os.environ.get['INPUT_BUCKET'] #these are automatically generated variables
+    bucket = os.environ.get('INPUT_BUCKET') #these are automatically generated variables
     bucket_region = os.environ['INPUT_BUCKET_REGION']
     dist_folder = os.environ['INPUT_DIST_FOLDER']
     
@@ -16,8 +16,9 @@ def deploy_s3_docker():
             s3_client.upload_file(os.path.join(root, file), bucket, file)
             
     website_url = f'https://{bucket}.s3-website-{bucket_region}.amazonaws.com'
-    print(f'::set-output name=website_url::{website_url}')
-    # set output command to set website url output of action to this dynamic value
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as f:
+        f.write(f'website-url={website_url}\n')
+    # set output using the current GITHUB_OUTPUT env file (::set-output is deprecated)
     
 if __name__ == '__main__':
     deploy_s3_docker()
